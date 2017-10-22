7731945697a838af46de2ffa7226509eafb3951e
Jessica Del Grande <jessica.delgrande@gmail.com>
2017-10-22 16:25:58 -0400

    first

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,10 +1,42 @@
+function autoType(elementClass, typingSpeed) {
+	let thhis = $(elementClass);
+	thhis.css({
+		"position": "relative",
+		"display": "inline-block"
+	});
+	// adding a dom element with a class of cursor
+	thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+	thhis = thhis.find(".detailsText");
+	let text = thhis.text().trim().split('');
+	let charNum = text.length;
+	let newString = "";
+	thhis.text("|");
+	setTimeout(function() {
+		thhis.css("opacity", 1);
+		thhis.prev().removeAttr("style");
+		thhis.text("");
+		for (let i = 0; i < charNum; i++) {
+			(function(i, char) {
+				setTimeout(function() {
+					newString += char;
+					thhis.text(newString);
+				}, i * typingSpeed);
+			})(i + 1, text[i]);
+		}
+	}, 1500);
+}
 
+// when user hovers over a specific detailsIcon, 
+// corresponding details div will begin autoType
+// $(".detailsIcon-twitter").hover(function() {
+// 	$(this).css(autoType(".details-twitter", 200),
+// 		)
+// });
 
-
-App.init = () => {
-	App.events();
-} //end of init();
-
-$(function() {
-	App.init();
+$(document).ready(function() { 
+	// calling the autoType function
+	// number is the speed that each letter is typed 
+	// autoType(".details", 200);
 });
+
+// need to call autoType on hover of individual icons
